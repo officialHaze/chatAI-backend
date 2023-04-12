f591689ba348747b93d7aa7e689ba4262eb0f367
Moinak <moinak.dey8@gmail.com>
2023-04-13 02:47:47 +0530

    changed the AI model

--- core/settings.py
+++ core/settings.py
@@ -96,18 +96,18 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#     'default': dj_database_url.config(
-#         default=os.environ.get('DEFAULT_DB_URL'),
-#         conn_max_age=600)
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
+# }
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DEFAULT_DB_URL'),
+        conn_max_age=600)
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators

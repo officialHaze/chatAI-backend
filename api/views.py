70d2fcd7ac3436e25df0e1fec6f53ef8ad52404a
Moinak <moinak.dey8@gmail.com>
2023-04-13 02:27:18 +0530

    changed the AI model

--- api/views.py
+++ api/views.py
@@ -11,23 +11,39 @@ from rest_framework import permissions
 load_dotenv()
 
 openai.api_key = os.environ.get("OPENAI_API_KEY")
+chat_completion_endpoint = os.environ.get('OPEN_AI_CHAT_COMPLETION_ENDPOINT')
 ASSEMBLY_AI_API_TOKEN = os.environ.get("ASSEMBLY_AI_API_TOKEN")
 
+prompt_array = [
+    '\nHuman:hello\nAI:Hi, how may i assist you today',
+    '\nHuman:who are you?\nAI:I am your friendly neighbourhood AI. I was created by OpenAI and is now being used in this web application developed by Moinak Dey',
+    '\nHuman:what is your name?\nAI:You can call me anything you like'
+    ]
+
+ai_res = None
 
 @api_view(["POST"])
 @permission_classes([permissions.IsAuthenticated])
 def get_ai_response(req, *args, **kwargs):
     data = req.data.get("message")
+    prompt = data[-1]['content'] #getting the last object from the array to get hold of the last message user sent
+    new_prompt = [*prompt_array, f'\nHuman:{prompt}\nAI: '] #takes the prompt array containing user message and ai response then appending the new user prompt to it for the AI to respond to
 
-    # try:
-    res = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo-0301",
-        messages=data
+    res = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=
+        f"The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly. {new_prompt}",
+        temperature=0.9,
+        max_tokens=150,
+        top_p=1,
+        frequency_penalty=0.0,
+        presence_penalty=0.6,
+        stop=[" Human:", " AI:"]
     )
-    message = res['choices'][0]['message']['content']
-    return Response({"message":message}, status=200)
-    # except:
-    #     return Response({"message":"error"}, status=400)
+    ai_res = res['choices'][0]['text'] #getting the AI response
+    prompt_array.append(f'\nHuman:{prompt}\nAI:{ai_res}') #appending it to the prompt_array(updating the previous prompt array)
+    return Response({"message":ai_res}, status=200)
+
 
 
 @api_view(["POST"])
